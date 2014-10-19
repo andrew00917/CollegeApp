fb6f2892b33587c9e5e26de5f7fcee128e2a386d
jhchoe <andrew00917@yahoo.com>
2014-10-19 13:31:05 -0400

    merge try

--- app/src/main/java/com/techhab/collegeapp/MainActivity.java
+++ app/src/main/java/com/techhab/collegeapp/MainActivity.java
@@ -21,7 +21,7 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
-    
+
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
