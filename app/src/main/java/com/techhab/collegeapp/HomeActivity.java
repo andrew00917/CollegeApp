90845bbfc5848277c09da954d69ba4c8d2640350
jhchoe <andrew00917@yahoo.com>
2014-10-23 22:07:41 -0400

    animation

--- app/src/main/java/com/techhab/collegeapp/HomeActivity.java
+++ app/src/main/java/com/techhab/collegeapp/HomeActivity.java
@@ -11,15 +11,9 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
-<<<<<<< HEAD
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
-=======
->>>>>>> dev_andrew
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
