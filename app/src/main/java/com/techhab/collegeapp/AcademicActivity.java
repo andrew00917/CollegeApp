f3f4b9a3b8383b950e70304d17b59758275785c6
jhchoe <andrew00917@yahoo.com>
2014-11-16 13:56:59 -0500

    added PagerTabSlider

--- app/src/main/java/com/techhab/collegeapp/AcademicActivity.java
+++ app/src/main/java/com/techhab/collegeapp/AcademicActivity.java
@@ -1,14 +1,16 @@
 package com.techhab.collegeapp;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.app.NavUtils;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,17 +18,13 @@ import android.view.MenuItem;
 public class AcademicActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    // When requested, this adapter returns a DemoObjectFragment,
-    // representing an object in the collection.
-    private CollectionPagerAdapter mCollectionPagerAdapter;
-    private ViewPager mViewPager;
+    private final Handler handler = new Handler();
 
-    // Fragment attributes
-    private static final int COURSE_SEARCH = 0;
-    private static final int CALENDAR = 1;
-    private static final int DRAWER = 2;
-    private static final int FRAGMENT_COUNT = DRAWER + 1;
-    private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+    Toolbar toolbar;
+    PagerSlidingTabStrip tabs;
+    ViewPager pager;
+
+    private MyPagerAdapter adapter;
 
     public AcademicActivity() {
         super();
@@ -36,12 +34,21 @@ public class AcademicActivity extends ActionBarActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academic);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        pager = (ViewPager) findViewById(R.id.pager);
         DrawerLayout mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        adapter = new MyPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        tabs.setViewPager(pager);
+
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
+                .getDisplayMetrics());
+        pager.setPageMargin(pageMargin);
     }
 
     @Override
@@ -77,36 +84,51 @@ public class AcademicActivity extends ActionBarActivity
     public void onNavigationDrawerItemSelected(int position) {
 
     }
-}
 
-// Since this is an object collection, use a FragmentStatePagerAdapter,
-// and NOT a FragmentPagerAdapter.
-class CollectionPagerAdapter extends FragmentStatePagerAdapter {
-
-    private FragmentManager fm;
-
-    public CollectionPagerAdapter(FragmentManager fm) {
-        super(fm);
-        this.fm = fm;
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+//        outState.putInt("currentColor", currentColor);
     }
 
     @Override
-    public Fragment getItem(int i) {
-//        Fragment fragment = new ObjectFragment();
-//        Bundle args = new Bundle();
-//        // Our object is just an integer :-P
-//        args.putInt(ObjectFragment.ARG_OBJECT, i + 1);
-//        fragment.setArguments(args);
-        return fm.findFragmentById(R.id.calendarFragment);
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+//        currentColor = savedInstanceState.getInt("currentColor");
+//        changeColor(currentColor);
     }
 
-    @Override
-    public int getCount() {
-        return 100;
-    }
+    /**
+     * MyPagerAdapter
+     */
+    public class MyPagerAdapter extends FragmentPagerAdapter {
 
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return "OBJECT " + (position + 1);
+        private final String[] TITLES = {"Search Courses", "Calendar"};
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new CourseSearchFragment();
+                case 1:
+                    return new CalendarFragment();
+                default:
+                    return new CourseSearchFragment();
+            }
+        }
     }
 }
