a61752f8cb248a4d37ba4c83e008c3e2496e9cba
gsmalley10 <griffin.smalley@gmail.com>
2014-12-09 12:17:38 -0500

    Almost got the sports activity working

--- app/src/main/java/com/techhab/collegeapp/AthleticActivity.java
+++ app/src/main/java/com/techhab/collegeapp/AthleticActivity.java
@@ -1,6 +1,5 @@
 package com.techhab.collegeapp;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -12,12 +11,16 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.techhab.collegeapp.application.CollegeApplication;
 
 
 public class AthleticActivity extends ActionBarActivity
@@ -30,12 +33,9 @@ public class AthleticActivity extends ActionBarActivity
     ViewPager pager;
     Switch boyGirlSwitch;
 
-    private MyPagerAdapter adapter;
+    private pagerAdapter adapter;
 
-    private SharedPreferences prefs;
-
-    // Has the user chosen between men's or women's sports
-//    private
+    private CollegeApplication application;
 
     private int currentPosition;
 
@@ -46,6 +46,8 @@ public class AthleticActivity extends ActionBarActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_athletic);
+
+        application = (CollegeApplication) getApplication();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
@@ -61,31 +63,68 @@ public class AthleticActivity extends ActionBarActivity
         boyGirlSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    adapter = new MyPagerAdapter(getSupportFragmentManager(), boysSports);
+                    adapter = new pagerAdapter(getSupportFragmentManager(), boysSports);
 //                    finish();
 //                    startActivity(getIntent());
                 } else {
-                    adapter = new MyPagerAdapter(getSupportFragmentManager(), girlsSports);
+                    adapter = new pagerAdapter(getSupportFragmentManager(), girlsSports);
 //                    finish();
 //                    startActivity(getIntent());
                 }
             }
         });
 
-
-
-        if ( boyGirlSwitch.isChecked() ) {
-            adapter = new MyPagerAdapter(getSupportFragmentManager(), girlsSports);
+        /* Check to see if this is the first time the user has opened
+        AthleticActivity. If it is, show the dialog to choose
+        Men's or Women's sports */
+        if ( application.getSportsFirstTime() ) {
+            Log.d("Open sports for first time", "true!");
+            new MaterialDialog.Builder(this)
+                    .title("View Sports For...")
+                    .items(new String[]{"Men", "Women"})
+                    .itemsCallbackSingleChoice(-1, new MaterialDialog.ListCallback() {
+                        @Override
+                        public void onSelection(MaterialDialog dialog, View view,
+                                                int which, CharSequence text) {
+                            if (which == 0) {
+                                application.setSportsPreference(true);
+                                Log.d("Sports Preferences", "I chose men");
+                                adapter.notifyDataSetChanged();
+//                                finish();
+//                                startActivity(getIntent());
+                            } else {
+                                application.setSportsPreference(false);
+                                Log.d("Sports Preferences", "I chose women");
+                                adapter.notifyDataSetChanged();
+//                                finish();
+//                                startActivity(getIntent());
+                            }
+                        }
+                    })
+                    .positiveText("Choose")
+                    .positiveColor(getResources().getColor(R.color.kzooOrange))
+                    .cancelable(false)
+                    .show();
         } else {
-            adapter = new MyPagerAdapter(getSupportFragmentManager(), boysSports);
+            Log.d("Open for first time", "false!");
+        }
+
+
+        // Check the user's sportsPreference SharedPreference to load to the correct adapter
+        if ( application.getSportsPreference() ) {
+            adapter = new pagerAdapter(getSupportFragmentManager(), boysSports);
+            Log.d("Adapter setter", "I already set the adapter to boysSports");
+        } else {
+            adapter = new pagerAdapter(getSupportFragmentManager(), girlsSports);
+            Log.d("Adapter setter", "I already set the adapter to girlsSports");
         }
 
 //        adapter = new MyPagerAdapter(getSupportFragmentManager(), boysSports);
         pager.setAdapter(adapter);
         tabs.setViewPager(pager);
 
-        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
-                .getDisplayMetrics());
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4,
+                getResources().getDisplayMetrics());
         pager.setPageMargin(pageMargin);
 
     }
@@ -100,7 +139,8 @@ public class AthleticActivity extends ActionBarActivity
         // Disable app name in toolbar
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,toolbar ,  R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
@@ -162,13 +202,13 @@ public class AthleticActivity extends ActionBarActivity
     }
 
     /**
-     * MyPagerAdapter
+     * pagerAdapter for the SlidingPageTab thing
      */
-    public class MyPagerAdapter extends FragmentPagerAdapter {
+    public class pagerAdapter extends FragmentPagerAdapter {
 
         private String[] TITLES;
 
-        public MyPagerAdapter(FragmentManager fm, String[] titles) {
+        public pagerAdapter(FragmentManager fm, String[] titles) {
             super(fm);
             this.TITLES = titles;
         }
@@ -184,32 +224,48 @@ public class AthleticActivity extends ActionBarActivity
         }
 
         @Override
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
+
+        @Override
         public Fragment getItem(int position) {
             Fragment fragment;
             Bundle args = new Bundle();
-            switch (position) {
-                case 0:
-                    fragment = new HomeGamesFragment();
-                    args.putInt(HomeGamesFragment.ARG_OBJECT, position + 1);
-                    fragment.setArguments(args);
-                    break;
-                case 1:
-                    fragment = new CalendarFragment();
-                    args.putInt(CalendarFragment.ARG_OBJECT, position + 1);
-                    fragment.setArguments(args);
-                    break;
-                case 2:
-                    fragment = new BasketballFragment();
-                    args.putInt(BasketballFragment.ARG_OBJECT, position + 1);
-                    fragment.setArguments(args);
-                    break;
-                default:
-                    fragment = new CalendarFragment();
-                    args.putInt(CalendarFragment.ARG_OBJECT, position + 1);
-                    fragment.setArguments(args);
-                    break;
+            if ( application.getSportsPreference() ) {
+                switch (position) {
+                    case 0:
+                        fragment = new HomeGamesFragment();
+                        args.putInt(HomeGamesFragment.ARG_OBJECT, position + 1);
+                        fragment.setArguments(args);
+                        break;
+                    case 1:
+                        fragment = new HomeGamesFragment();
+                        args.putInt(HomeGamesFragment.ARG_OBJECT, position + 1);
+                        fragment.setArguments(args);
+                        break;
+                    case 2:
+                        fragment = new BasketballFragment();
+                        args.putInt(BasketballFragment.ARG_OBJECT, position + 1);
+                        fragment.setArguments(args);
+                        break;
+                    default:
+                        fragment = new HomeGamesFragment();
+                        args.putInt(HomeGamesFragment.ARG_OBJECT, position + 1);
+                        fragment.setArguments(args);
+                        break;
+                }
+                return fragment;
+            } else {
+                switch (position) {
+                    default:
+                        fragment = new CalendarFragment();
+                        args.putInt(CalendarFragment.ARG_OBJECT, position + 1);
+                        fragment.setArguments(args);
+                        break;
+                }
+                return fragment;
             }
-            return fragment;
         }
     }
 }
