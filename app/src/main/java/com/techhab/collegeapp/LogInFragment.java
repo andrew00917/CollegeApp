0c94bc820949e18e62e3e7c1836d49629e9b0ad3
jhchoe <andrew00917@yahoo.com>
2015-02-14 18:33:14 -0500

    login fragment update

--- app/src/main/java/com/techhab/collegeapp/LogInFragment.java
+++ app/src/main/java/com/techhab/collegeapp/LogInFragment.java
@@ -1,6 +1,7 @@
 package com.techhab.collegeapp;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.Fragment;
@@ -16,6 +17,16 @@ import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.techhab.collegeapp.application.CollegeApplication;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
 
 
 public class LogInFragment extends Fragment {
@@ -65,14 +76,17 @@ public class LogInFragment extends Fragment {
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                kEmail.setError("Invalid email or password");
+                if (kEmail.getText().toString().length() < 7 || password.getText().toString().length() < 3) {
+                    showLoginError();
+                } else {
+                    loginSubmit();
+                }
             }
         });
 
         rememberMeText = (TextView) v.findViewById(R.id.remember_me_text);
 
-        numChar = kEmail.getText().toString().length();
-
+        //numChar = kEmail.getText().toString().length();
 
         rememberMeText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,39 +102,51 @@ public class LogInFragment extends Fragment {
         guestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                login_submit();
+                loginSubmit();
             }
         });
 
         return v;
     }
 
-    private void login_submit() {
+    private void loginSubmit() {
         String u = kEmail.getText().toString();
         String p = password.getText().toString();
 
         try {
-            User newUser = new User(u, p);
             if (u.equals("guest")) {
                 application.setIsSocial(false);
+            } else {
+                User newUser = new User(u, p);
+                application.setCurrentUser(newUser);
+                new MySqlAsync().execute(u, p);
             }
-            application.setCurrentUser(newUser);
-            application.load();
-            if (application.isLoggedIn()) {
-                closeKeyboard(getActivity(), kEmail.getWindowToken());
-                closeKeyboard(getActivity(), password.getWindowToken());
-                ((HomeActivity) getActivity()).showFragment(HOME_FRAGMENT, false);
-                ((HomeActivity) getActivity()).mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-                ((HomeActivity) getActivity()).getSupportActionBar().show();
-            }
-            else {
-                // exception
-                ((HomeActivity) getActivity()).showFragment(LOGIN_HOME_FRAGMENT, false);
-            }
-            application.save();
         } catch (Exception e) {
             e.printStackTrace();
             // alert user for an error
+        }
+    }
+
+    private void postLogin(String result) {
+        User user = application.getCurrentUser();
+        if (result.length() > 0) {
+            // String[] info = result.split(",");
+
+            user.setValid(true);
+
+            application.setRememberLogin(rememberMeCheckBox.isChecked());
+            application.setLoggedIn(true);
+            application.setIsSocial(true);
+            application.save();
+
+            closeKeyboard(getActivity(), kEmail.getWindowToken());
+            closeKeyboard(getActivity(), password.getWindowToken());
+            ((HomeActivity) getActivity()).showFragment(HOME_FRAGMENT, false);
+            ((HomeActivity) getActivity()).mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+            ((HomeActivity) getActivity()).getSupportActionBar().show();
+        } else {
+            user.setUserAsGuest();
+            showLoginError();
         }
     }
 
@@ -133,5 +159,54 @@ public class LogInFragment extends Fragment {
     public static void closeKeyboard(Context c, IBinder windowToken) {
         InputMethodManager mgr = (InputMethodManager) c.getSystemService(Context.INPUT_METHOD_SERVICE);
         mgr.hideSoftInputFromWindow(windowToken, 0);
+    }
+
+    private void showLoginError() {
+        kEmail.setError("Invalid email or password");
+    }
+
+    /**
+     * Class: MySqlAsync
+     *
+     * Connect to mySQL via php using getMethod.
+     * Use HttpGet and HttpClient to connect.
+     */
+    public class MySqlAsync extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String...params) {
+            String u = (String) params[0];
+            String p = (String) params[1];
+
+            String link = "http://www.techhab.com/sample_query.php?useremail=" + u + "&password=" + p;
+
+            try {
+                URL url = new URL(link);
+                HttpClient client = new DefaultHttpClient();
+                HttpGet request = new HttpGet();
+                request.setURI(new URI(link));
+
+                HttpResponse response = client.execute(request);
+                BufferedReader in = new BufferedReader
+                        (new InputStreamReader(response.getEntity().getContent()));
+
+                StringBuffer sb = new StringBuffer("");
+                String line="";
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                }
+                in.close();
+
+                return sb.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            postLogin(result);
+        }
     }
 }
